c34a91cad7d3b7113ef41f81cc6d54382a517165
LindaniMags <lindanimags@gmail.com>
2024-01-08 13:42:16 -0800

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for left in matrix:
+        for right in left:
+            print("{:d}".format(right), last=" " if right != left[-1] else "")
+        print()
