9945a88791117038c8433c62bb933bd0d2dd11a7
LindaniMags <lindanimags@gmail.com>
2024-01-22 12:45:04 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[el]), end="")
             total += 1
         except (ValueError, TypeError, IndexError):
-            break
+            continue
     print()
     return total
