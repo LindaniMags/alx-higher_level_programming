9b00f8f942cfcf1573e36c9add209573f6c1cfb4
LindaniMags <lindanimags@gmail.com>
2024-01-31 18:25:33 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""Class withouth attributes"""
+"""Class withouth attributes."""
 
 class LockedClass:
-    """Prevents user from creating new attributes"""
+    """
+    Prevents user from creating new attributes.
+
+    Attributes:
+        first-name: First name string.
+    """
 
     __slots__ = ["first_name"]
