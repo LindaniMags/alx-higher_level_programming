9c6516546c5c8c32bcd3e0dcbc77b3e02c8ea755
LindaniMags <lindanimags@gmail.com>
2024-01-31 16:29:16 -0800

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""empty class Rectangle that defines a rectangle"""
+"""Empty class Rectangle that defines a rectangle"""
+
 
 class Rectangle:
-    """empty class Rectangle that defines a rectangle"""
+    """Empty Rectangle class"""
     pass
