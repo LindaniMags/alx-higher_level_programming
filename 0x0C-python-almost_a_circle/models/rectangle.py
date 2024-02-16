7edfb3621aca1662270dc11c74d40cf432a2f572
LindaniMags <lindanimags@gmail.com>
2024-02-16 06:01:50 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,18 +2,17 @@
 """
 Rectangle class.
 """
-from base import Base
+from task1 import Base
 
 class Rectangle(Base):
     """Define Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Rectangle class constructor"""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        self.id = id
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -49,4 +48,13 @@ class Rectangle(Base):
         """Y setter"""
         self.__y = y
 
+if __name__ == "__main__":
 
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
