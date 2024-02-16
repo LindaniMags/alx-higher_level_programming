b2b8d90d0c19d7d05a626cb132dfa432621c1717
LindaniMags <lindanimags@gmail.com>
2024-02-16 03:17:14 -0800

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""
-Defines the base class.
-"""
 
 
 class Base:
-    """Base class."""
+    """Base model.
+
+    This Represents the "base" for all other classes in project 0x0C*.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize Base class"""
-        if id is None:
-            Base.__nb_objects+=1
-            self.id = Base.__nb_objects
-        else:
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
