37239a3a6b84af6128ed0c61c6d106166c4a6596
LindaniMags <lindanimags@gmail.com>
2024-07-05 15:19:54 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-Python Script that takes in a URL, sends a request to the URL
-& displays the body of the response
-"""
+""" If the HTTP status code is greater than or equal to 400, print: Error code """
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    r = requests.get(argv[1])
-    status = r.status_code
-    print(r.text) if status < 400 else print(
-        "Error code: {}".format(r.status_code))
+    response = requests.get(argv[1])
+    status = response.status_code
+    print(response.text) if status < 400 else print(
+        f"Error code: {response.status_code}")
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" Sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter """
-import sys
+""" Sends a POST request to URL with a letter as a parameter. """
 import requests
+from sys import argv
 
-if __name__ == "__main__":
-    char = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": char}
-
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+if __name__ == '__main__':
+    q = argv[1] if len(argv) == 2 else ""
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data={'q': q})
     try:
-        response = r.json()
-        if response == {}:
+        r_json = r.json()
+        id, name = r_json.get('id'), r_json.get('name')
+        if len(r_json) == 0 or not id or not name:
             print("No result")
         else:
-            print(f"[{response.get("id")}] {response.get("name")}")
-    except ValueError:
+            print("[{}] {}".format(r_json.get('id'), r_json.get('name')))
+    except Exception:
         print("Not a valid JSON")
