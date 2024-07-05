e0cb97bd2edaa785fe221e2ca179a58e617d5ca4
LindaniMags <lindanimags@gmail.com>
2024-07-05 15:40:59 -0700

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""" Evaluates candidates applying for a back-end position """
-import sys
+"""
+Python script that takes 2 arguments in order to solve the given challenge
+"""
 import requests
+from sys import argv
 
-if __name__ == "__main__":
-    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
-
-    response = requests.get(url)
-    c = response.json()
-    try:
-        for i in range(10):
-            print(f"{c[i].get("sha")}: {c[i].get("commit").get("author").get("name")}")
-    except IndexError:
-        pass
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
