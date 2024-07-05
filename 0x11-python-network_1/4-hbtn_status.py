a85b1d6a7dac0b084c77749a5a291b041112fb16
LindaniMags <lindanimags@gmail.com>
2024-07-05 13:24:38 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,9 @@
 """fetches https://intranet.hbtn.io/status."""
 import requests
 
+
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t- type: {type(r.text)}")
     print(f"\t- type: {type(r.text)}")
     print(f"\t- content: {r.text}")
