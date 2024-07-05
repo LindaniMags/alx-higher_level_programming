79738649759474ec55018e471dc4c370530e1c57
LindaniMags <lindanimags@gmail.com>
2024-07-05 13:38:04 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,9 @@
 """ Fetches https://intranet.hbtn.io/status. """
 import requests
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+url = "https://alx-intranet.hbtn.io/status"
+response = requests.get(url)
+
+print("Body response:")
+print(f"\t- type: {type(response.text)}")
+print(f"\t- content: {response.text}")
