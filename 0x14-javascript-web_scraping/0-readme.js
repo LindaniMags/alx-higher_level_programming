9f20ac61a06a16b3d4b23d06219e98345e88a730
LindaniMags <lindanimags@gmail.com>
2024-07-30 07:52:28 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
-const fs = require("fs");
-
-// Get the file path from the command line arguments
-const filePath = process.argv[2];
-
-// Read the file content in utf-8 encoding
-fs.readFile(filePath, "utf8", (err, data) => {
-  if (err) {
-    // Print the error object if an error occurred
-    console.error("Error reading the file:", err);
-  } else {
-    // Print the content of the file
-    console.log(data);
-  }
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	console.log(error || content);
 });
-
