15da743f8e83c9c6f04869592784b90a25f21aa7
LindaniMags <lindanimags@gmail.com>
2024-07-30 06:06:27 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 
 fs.readFile(process.argv[2], "utf8", function (err, data) {
   if (err) {
-    return console.log(err);
+    return console.error(err);
   }
   console.log(data);
 });
+
