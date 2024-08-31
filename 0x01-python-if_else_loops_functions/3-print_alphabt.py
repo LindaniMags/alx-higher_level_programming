cbb836fc4f36b05677d699d2f2524e5b7496d766
LindaniMags <lindanimags@gmail.com>
2024-08-31 09:45:39 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-"""" prints the ASCII alphabet, in lowercase """
-for l in range(97, 123):
-    print("{}".format(chr(l)), end="")
+"""prints the ASCII alphabet, in lowercase, except q and e"""
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
